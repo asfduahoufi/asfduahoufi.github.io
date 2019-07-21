30ac44669f21c85de7f3f53ddcb874ce93fe12c3
asfduahoufi <53156081+asfduahoufi@users.noreply.github.com>
2019-07-21 18:34:02 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+/* d3.csv("example_tossups.csv").then(function(data) {
+  console.log(data[0]);
+});
+ */
+ 
+var cardRules = new Array;
+    $.get('example_tossups.csv', function(data){
+            var array = data.split('\n');
+            console.log(cardRules);
+        });
+
+var showText = function (target, message, index, interval) {    
+  if (index < message.length) { 
+    $(target).append(message[index++]); 
+    setTimeout(function () { showText(target, message, index, interval); }, interval); 
+  } 
+}
+    
+$(function () { 
+  showText("#msg", "Hello, World!", 0, 125);    
+}); 

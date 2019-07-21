b7238309f5aff51576959ec8209302b6be97c0d8
asfduahoufi <53156081+asfduahoufi@users.noreply.github.com>
2019-07-21 18:44:39 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,11 @@
  */
  
 var cardRules = new Array;
-    $.get('https://asfduahoufi.github.io/example_tossups.csv', function(data){
-            var array = data.split('\n');
-            console.log(cardRules);
-        });
+$.get('https://asfduahoufi.github.io/example_tossups.csv', function(data){
+		cardRules = data.split('\n');
+		console.log(cardRules);
+	});
+
 
 var showText = function (target, message, index, interval) {    
   if (index < message.length) { 
